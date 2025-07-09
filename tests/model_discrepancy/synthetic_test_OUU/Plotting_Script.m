90daf9139256d9a5706253fed185df8e41f3ea98
Hart <joshart@sandia.gov>
2025-07-09 17:00:30 -0400

    text problem with txt file in test problem

--- tests/model_discrepancy/synthetic_test_OUU/Plotting_Script.m
+++ tests/model_discrepancy/synthetic_test_OUU/Plotting_Script.m
@@ -64,9 +64,14 @@ diff = [diff;local_diff];
 
 post_delta_samples = cell(3,1);
 for k = 1:3
-    post_delta_samples{k} = zeros(51,100);
+    post_delta_samples{k} = zeros(1530,100);
     for j = 1:100
-        post_delta_samples{k}(:,j) = load(['posterior_discrepancy_samples_',num2str(k),'/Vector_',num2str(j),'.txt']);
+        tmp1 = zeros(51,30);
+        for s = 1:30
+            tmp1(:,s) = load(['posterior_discrepancy_samples_',num2str(k),'/Vector_',num2str(j),'_ens_',num2str(s),'.txt']);
+        end
+        tmp1 = tmp1';
+        post_delta_samples{k}(:,j) = tmp1(:);
     end
 end
 post_delta_samples_sabl = load('Sabl_Output.mat','post_delta_samples').post_delta_samples;
@@ -96,36 +101,40 @@ if ~surpress_figures
 
     x = linspace(0,1,51)';
 
+    u = reshape(prior_delta_z_opt(:,1),30,51)';
+    u_sabl = reshape(prior_delta_z_opt_sabl(:,1),30,51)';
     figure,
-    plot(x,prior_delta_z_opt,'LineWidth',3)
+    plot(x,u,'LineWidth',3)
     title('HdsaLib')
     figure,
-    plot(x,prior_delta_z_opt_sabl,'LineWidth',3)
+    plot(x,u_sabl,'LineWidth',3)
     title('Sabl')
     pause()
     close all
 
-    for k = 1:5
+    for k = 1:3
+        u = reshape(prior_delta{10}(:,k),30,51)';
+        u_sabl = reshape(prior_delta_sabl{10}(:,k),30,51)';
         figure,
-        plot(x,prior_delta{k},'LineWidth',3)
+        plot(x,u,'LineWidth',3)
         title('HdsaLib')
         figure,
-        plot(x,prior_delta_sabl{k},'LineWidth',3)
+        plot(x,u_sabl,'LineWidth',3)
         title('Sabl')
         pause()
         close all
     end
 
     for k = 1:3
+        u = reshape(post_delta_samples{k}(:,20),30,51)';
+        u_sabl = reshape(post_delta_samples_sabl{k}(:,20),30,51)';
         figure
         hold on
-        plot(x,post_delta_samples{k},'LineWidth',3,'Color',.9*ones(3,1))
-        plot(x,post_delta_mean(:,k),'LineWidth',3,'Color','red')
+        plot(x,u,'LineWidth',3,'Color',.9*ones(3,1))
         title('HdsaLib')
         figure
         hold on
-        plot(x,post_delta_samples_sabl{k},'LineWidth',3,'Color',.9*ones(3,1))
-        plot(x,post_delta_mean_sabl(:,k),'LineWidth',3,'Color','red')
+        plot(x,u_sabl,'LineWidth',3,'Color',.9*ones(3,1))
         title('Sabl')
         pause()
         close all
@@ -133,22 +142,14 @@ if ~surpress_figures
 
     figure,
     hold on
-    plot(x, (1 + x) / (1.2^(1 / 3)), 'color', 'black', 'LineWidth', 3)
-    plot(x, 1 + x, 'color', 'cyan', 'LineWidth', 3)
     plot(x,post_z_mean,'--','LineWidth',3,'Color','red')
     plot(x,post_z_samples,'LineWidth',3,'Color',.9*ones(3,1))
-    plot(x, (1 + x) / (1.2^(1 / 3)), 'color', 'black', 'LineWidth', 3)
-    plot(x, 1 + x, 'color', 'cyan', 'LineWidth', 3)
     plot(x,post_z_mean,'--','LineWidth',3,'Color','red')
     title('HdsaLib')
     figure,
     hold on
-    plot(x, (1 + x) / (1.2^(1 / 3)), 'color', 'black', 'LineWidth', 3)
-    plot(x, 1 + x, 'color', 'cyan', 'LineWidth', 3)
     plot(x,post_z_mean_sabl,'--','LineWidth',3,'Color','red')
     plot(x,post_z_samples_sabl,'LineWidth',3,'Color',.9*ones(3,1))
-    plot(x, (1 + x) / (1.2^(1 / 3)), 'color', 'black', 'LineWidth', 3)
-    plot(x, 1 + x, 'color', 'cyan', 'LineWidth', 3)
     plot(x,post_z_mean_sabl,'--','LineWidth',3,'Color','red')
     title('Sabl')
 
