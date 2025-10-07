f90c4cb48ec92ad1eb4d2a64b146e491982bbb14
Hart <joshart@sandia.gov>
2025-10-07 14:41:08 -0400

    updating example to use the mean plus AVaR objective

--- examples/mrhyde/thermal/2D_stoch_opt/cvar_obj_compare/Compare_Objectives.py
+++ examples/mrhyde/thermal/2D_stoch_opt/cvar_obj_compare/Compare_Objectives.py
@@ -3,19 +3,19 @@ import pdb
 
 lofi_objs = np.loadtxt('sample_obj_vals_lofi.dat')
 lofi_objs.sort()
-lofi_cvar = lofi_objs[-4:].mean()
+lofi_obj = 0.5 * lofi_objs[-4:].mean() + 0.5 * lofi_objs.mean()
 
 update_objs = np.loadtxt('sample_obj_vals_mean_update.dat')
 update_objs.sort()
-update_cvar = update_objs[-4:].mean()
+update_obj = 0.5 * update_objs[-4:].mean() + 0.5 * update_objs.mean()
 
 hifi_objs = np.loadtxt('sample_obj_vals_hifi.dat')
 hifi_objs.sort()
-hifi_cvar = hifi_objs[-4:].mean()
+hifi_obj = 0.5 * hifi_objs[-4:].mean() + 0.5 * hifi_objs.mean()
 
 filename = 'cvar_objectives.txt'
 with open(filename, 'w') as file:
-    file.write(f'CVaR objective at low-fidelity: {lofi_cvar}\n')
-    file.write(f'CVaR objective at mean update: {update_cvar}\n')
-    file.write(f'CVaR objective at high-fidelity: {hifi_cvar}\n')
+    file.write(f'CVaR objective at low-fidelity: {lofi_obj}\n')
+    file.write(f'CVaR objective at mean update: {update_obj}\n')
+    file.write(f'CVaR objective at high-fidelity: {hifi_obj}\n')
 
