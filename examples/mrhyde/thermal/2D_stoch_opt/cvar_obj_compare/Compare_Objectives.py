dc2293be5dbd8194b0ca2d333dad62ca596e321b
Hart <joshart@sandia.gov>
2025-10-21 13:40:04 -0400

    updating the python script for cvar objective comparisons

--- examples/mrhyde/thermal/2D_stoch_opt/cvar_obj_compare/Compare_Objectives.py
+++ examples/mrhyde/thermal/2D_stoch_opt/cvar_obj_compare/Compare_Objectives.py
@@ -1,17 +1,22 @@
 import numpy as np
 import pdb
 
+alpha = 0.5
+beta = 0.7
+
 lofi_objs = np.loadtxt('sample_obj_vals_lofi.dat')
+N = lofi_objs.shape[0]
+T = int(np.round(N*(1-beta)))
 lofi_objs.sort()
-lofi_obj = 0.5 * lofi_objs[-4:].mean() + 0.5 * lofi_objs.mean()
+lofi_obj = alpha * lofi_objs[-T:].mean() + (1-alpha) * lofi_objs.mean()
 
 update_objs = np.loadtxt('sample_obj_vals_mean_update.dat')
 update_objs.sort()
-update_obj = 0.5 * update_objs[-4:].mean() + 0.5 * update_objs.mean()
+update_obj = alpha * update_objs[-T:].mean() + (1-alpha) * update_objs.mean()
 
 hifi_objs = np.loadtxt('sample_obj_vals_hifi.dat')
 hifi_objs.sort()
-hifi_obj = 0.5 * hifi_objs[-4:].mean() + 0.5 * hifi_objs.mean()
+hifi_obj = alpha * hifi_objs[-T:].mean() + (1-alpha) * hifi_objs.mean()
 
 filename = 'cvar_objectives.txt'
 with open(filename, 'w') as file:
