c6920b4fe60e4939f456bed37748e163d8dfda15
Hart <joshart@sandia.gov>
2025-11-13 09:37:55 -0500

    minor postprocessing edit

--- examples/mrhyde/thermal/2D_stoch_opt/discretized_control/cvar_obj_compare/extract_hdsa_z_data.py
+++ examples/mrhyde/thermal/2D_stoch_opt/discretized_control/cvar_obj_compare/extract_hdsa_z_data.py
@@ -5,6 +5,9 @@ import subprocess
 
 ########## This block contains the variables/function specified by the user on a case-by-case basis #############################
 
+lofi_opt = np.loadtxt('../optimization/final_params_.dat', skiprows=2, max_rows=441)
+mask = np.abs(lofi_opt) < 1.e-15
+
 # Specify the variable name assigned to the optimization variable in the exodus file
 variable_name = 'thermal_boundary_control'
 
@@ -18,6 +21,8 @@ output_file = 'final_params_load_.txt'  # Base name for the output files
 exo_in = exodusii.File(input_file, mode="r")
 
 # Apply a node-wise transformation (example: square the values)
-lofi_opt = exo_in.get_node_variable_values(variable_name).squeeze()
+mean_opt = exo_in.get_node_variable_values(variable_name).squeeze()
 
-np.savetxt(output_file, lofi_opt, fmt='%.2f') 
+mean_opt[mask] = 0.0
+
+np.savetxt(output_file, mean_opt, fmt='%.2f') 
