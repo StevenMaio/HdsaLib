df3313b53c0b55f75119b6f4acc12f35081d5116
Hart <joshart@sandia.gov>
2026-04-22 08:36:21 -0400

    minor naming edit

--- src/core/model_discrepancy/interfaces/ouu_interfaces/HDSA_MD_OUU_Hyperparameter_Data_Interface.hpp
+++ src/core/model_discrepancy/interfaces/ouu_interfaces/HDSA_MD_OUU_Hyperparameter_Data_Interface.hpp
@@ -18,10 +18,10 @@ namespace HDSA
 
   private:
     int ens_size_;
-    HDSA::Ptr<MD_OUU_Data_Interface<RealT>> md_ouu_data_interface_;
+    HDSA::Ptr<MD_OUU_Data_Interface<RealT>> ouu_data_interface_;
 
   public:
-    MD_OUU_Hyperparameter_Data_Interface(HDSA::Ptr<MD_OUU_Data_Interface<RealT>> &md_ouu_data_interface) : ens_size_(md_ouu_data_interface->Get_Ensemble_Size()), md_ouu_data_interface_(md_ouu_data_interface)
+    MD_OUU_Hyperparameter_Data_Interface(HDSA::Ptr<MD_OUU_Data_Interface<RealT>> &ouu_data_interface) : ens_size_(ouu_data_interface->Get_Ensemble_Size()), ouu_data_interface_(ouu_data_interface)
     {
     }
 
@@ -31,11 +31,11 @@ namespace HDSA
 
     HDSA::Ptr<HDSA::Vector<RealT>> Load_Optimal_u(void) const
     {
-      HDSA::Ptr<HDSA::Vector<RealT>> u_opt = md_ouu_data_interface_->Load_Optimal_us(0)->Clone();
+      HDSA::Ptr<HDSA::Vector<RealT>> u_opt = ouu_data_interface_->Load_Optimal_us(0)->Clone();
       u_opt->Zeros();
       for (int s = 0; s < ens_size_; s++)
       {
-        HDSA::Ptr<HDSA::Vector<RealT>> us_opt = md_ouu_data_interface_->Load_Optimal_us(s);
+        HDSA::Ptr<HDSA::Vector<RealT>> us_opt = ouu_data_interface_->Load_Optimal_us(s);
         u_opt->Plus(*us_opt);
       }
       u_opt->Scale(1.0 / static_cast<RealT>(ens_size_));
@@ -44,22 +44,22 @@ namespace HDSA
 
     HDSA::Ptr<HDSA::Vector<RealT>> Load_Optimal_z(void) const
     {
-      HDSA::Ptr<HDSA::Vector<RealT>> z_opt = md_ouu_data_interface_->Load_Optimal_z();
+      HDSA::Ptr<HDSA::Vector<RealT>> z_opt = ouu_data_interface_->Load_Optimal_z();
       return z_opt;
     }
 
     HDSA::Ptr<HDSA::MultiVector<RealT>> Load_Z_Data(void) const
     {
-      HDSA::Ptr<HDSA::MultiVector<RealT>> Z = md_ouu_data_interface_->Load_Z_Data();
+      HDSA::Ptr<HDSA::MultiVector<RealT>> Z = ouu_data_interface_->Load_Z_Data();
       return Z;
     }
 
     HDSA::Ptr<HDSA::MultiVector<RealT>> Load_D_Data(void) const
     {
-      HDSA::Ptr<HDSA::MultiVector<RealT>> D = md_ouu_data_interface_->Load_Ds_Data(0)->Clone();
+      HDSA::Ptr<HDSA::MultiVector<RealT>> D = ouu_data_interface_->Load_Ds_Data(0)->Clone();
       for (int s = 0; s < ens_size_; s++)
       {
-        D->Plus(*md_ouu_data_interface_->Load_Ds_Data(s));
+        D->Plus(*ouu_data_interface_->Load_Ds_Data(s));
       }
       D->Scale(1.0 / static_cast<RealT>(ens_size_));
       return D;
